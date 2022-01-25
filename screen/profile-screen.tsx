4d34ba0d39091b7c8596e5987dacf7a744278d5c
joey kwok <kwokjoey2000@gmail.com>
2022-01-25 16:04:53 +0800

    feat: import styled component

--- screen/profile-screen.tsx
+++ screen/profile-screen.tsx
@@ -1,9 +1,16 @@
 import { observer } from 'mobx-react'
+import styled from 'styled-components'
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+const Root = styled.View`
+  flex : 1;
+  align-items : center;
+  justify-content : center;
+`
 
 export const ProfileScreen = observer(() =>
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  <Root>
     <Text>Profile Screen</Text>
-  </View>
+  </Root>
 );
