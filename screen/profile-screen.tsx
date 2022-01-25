e66f7ca6b2a900abb8baee5fa1a5a554192dd5a3
joey kwok <kwokjoey2000@gmail.com>
2022-01-25 16:14:47 +0800

    feat: update profile screen to detect login status

--- screen/profile-screen.tsx
+++ screen/profile-screen.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react'
 import styled from 'styled-components'
 import * as React from 'react';
 import { Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const Root = styled.View`
   flex : 1;
@@ -9,8 +10,33 @@ const Root = styled.View`
   justify-content : center;
 `
 
-export const ProfileScreen = observer(() =>
-  <Root>
-    <Text>Profile Screen</Text>
-  </Root>
+export const ProfileScreen = observer(() => {
+  const [initializing, setInitializing] = React.useState(true);
+  const [user, setUser] = React.useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  React.useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  return (
+    user != null ?
+      <Root>
+        < Text > Welcome {user.email}</Text >
+      </Root >
+      :
+      <Root>
+        <Text>Please Login</Text>
+      </Root>
+
+  );
+
+}
 );
