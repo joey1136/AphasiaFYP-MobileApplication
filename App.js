33d609154f381e4d63e313b6adcc2d8a44efcf60
joey kwok <kwokjoey2000@gmail.com>
2022-03-07 17:25:31 +0800

    fix:missing android setting for icon

--- App.js
+++ App.js
@@ -5,14 +5,27 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ProfileNavigator } from './src/screen'
 import { t } from './src/language-pack/language';
 import HomeNavigator from './src/screen/home-navigator';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import styled from 'styled-components';
 
 const Tab = createBottomTabNavigator();
 
+const SaveIcon = styled(MaterialCommunityIcon)`
+  font-size:25px;
+`
 const App = observer(() =>
   <NavigationContainer >
     <Tab.Navigator >
-      <Tab.Screen options={{ headerShown: false }} name={t.homeScreen.title} component={HomeNavigator} />
-      <Tab.Screen options={{ headerShown: false }} name={t.profileScreen.title} component={ProfileNavigator} />
+      <Tab.Screen options={{
+        headerShown: false, tabBarIcon: ({ color, size }) => (
+          <SaveIcon name="home" color={color} size={size} />
+        )
+      }} name={t.homeScreen.title} component={HomeNavigator} />
+      <Tab.Screen options={{
+        headerShown: false, tabBarIcon: ({ color, size }) => (
+          <SaveIcon name="account-circle" color={color} size={size} />
+        )
+      }} name={t.profileScreen.title} component={ProfileNavigator} />
     </Tab.Navigator>
   </NavigationContainer>
 );
